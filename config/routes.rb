d995efc4cafad44185e2b090296d27bc0bdee79e
Artur <artur@MacBook-Pro-Artur.local>
2018-03-12 18:55:59 +0000

    init9

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
   resources :pages, :only => [:index]
 
   root to: 'ads#index'
-  
+
   resources :user_sessions, :users, only: [:new, :create]
   delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
-  get '/sign_in', to: 'user_sessions#new', as: :sign_in# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/sign_in', to: 'user_sessions#new', as: :sign_in
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
